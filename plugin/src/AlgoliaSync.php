b9864adf3ce1ff00a1bcba83dd8269a1b7202091
Daniele Rosario <daniele@weble.it>
2022-11-23 12:59:28 +0100

    fix lang in urls

--- plugin/src/AlgoliaSync.php
+++ plugin/src/AlgoliaSync.php
@@ -10,14 +10,22 @@ use Category;
 use ElementTextareaPro;
 use ItemRenderer;
 use JFolder;
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Language;
 use Joomla\CMS\Language\LanguageHelper;
 use Joomla\CMS\Menu\MenuItem;
+use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\Router\Router;
+use Joomla\CMS\Uri\Uri;
 use Joomla\Filesystem\Folder;
 use Joomla\Registry\Registry;
+use Joomla\String\StringHelper;
 use stdClass;
 use Type;
+use PlgSystemLanguageFilter;
 
 class AlgoliaSync
 {
@@ -29,11 +37,25 @@ class AlgoliaSync
     private ?array $menuItems = null;
     private array $categories = [];
     private Application $application;
+    private CMSApplication $app;
+    /**
+     * @var mixed
+     */
+    private bool $mode_sef;
+    private array $lang_codes;
+    /**
+     * @var mixed|stdClass
+     */
+    private string $default_lang;
+    /**
+     * @var false|mixed|stdClass|string
+     */
+    private string $current_lang;
+    private Registry $params;
 
     public function __construct(\Type $type)
     {
         $this->application = $type->getApplication();
-
 
         $this->zoo = $this->application->app;
         $this->renderer = $this->zoo->renderer->create('item', ['path' => $this->zoo->path]);
@@ -45,10 +67,11 @@ class AlgoliaSync
             );
         }
 
-        if ($this->client && $this->application->getParams()->get('global.config.algolia_index_'. $type->identifier)) {
-            $this->index = $this->client->initIndex($this->application->getParams()->get('global.config.algolia_index_'. $type->identifier));
+        if ($this->client && $this->application->getParams()->get('global.config.algolia_index_' . $type->identifier)) {
+            $this->index = $this->client->initIndex($this->application->getParams()->get('global.config.algolia_index_' . $type->identifier));
         }
 
+        $this->loadRouterLanguageRules();
     }
 
     public function isConfigured(): bool
@@ -147,16 +170,16 @@ class AlgoliaSync
 
         $this->categories = $application->getCategoryTree();
         $data = [
-            'id'  => $item->id,
-            'url' => [],
-            'category_ids' => array_filter(array_merge($item->getRelatedCategoryIds(), $this->array_flatten(array_map(function($id) {
+            'id'           => $item->id,
+            'url'          => [],
+            'category_ids' => array_filter(array_merge($item->getRelatedCategoryIds(), $this->array_flatten(array_map(function ($id) {
                 /** @var Category $category */
                 $category = $this->categories[$id] ?? null;
                 if (!$category) {
                     return [];
                 }
 
-                return array_map(function($parent) {
+                return array_map(function ($parent) {
                     return $parent->id;
                 }, $category->getPathway());
 
@@ -187,7 +210,7 @@ class AlgoliaSync
             } else {
                 if (!is_array($data[$key])) {
                     $data[$key] = [$data[$key]];
-                    
+
                     $data[$key][] = $value;
                 }
             }
@@ -205,7 +228,10 @@ class AlgoliaSync
             }
         }
 
-        $data_override = $this->notify('onYooAlgoliaData', [$item, $data]);
+        $data_override = $this->notify('onYooAlgoliaData', [
+            $item,
+            $data
+        ]);
 
         if ($data_override != null) {
             return $data_override;
@@ -218,7 +244,10 @@ class AlgoliaSync
     {
         $value = null;
 
-        $value = $this->notify('onYooAlgoliaBeforeElementData', [$element, $params]);
+        $value = $this->notify('onYooAlgoliaBeforeElementData', [
+            $element,
+            $params
+        ]);
 
         if ($value !== null) {
             return $value;
@@ -356,7 +385,7 @@ class AlgoliaSync
                 }
             }
 
-           return $items;
+            return $items;
 
         }
 
@@ -446,7 +475,7 @@ class AlgoliaSync
             /** @var MenuItem $menu_item */
             foreach ($menu_items as $menu_item) {
                 /** @var Registry $menuItemParams */
-                $menuItemParams = $zoo->parameter->create($menu_item->params);
+                $menuItemParams = $menu_item->getParams();
                 $menuItemLanguage = $menu_item->language;
 
                 switch (@$menu_item->query['view']) {
@@ -636,7 +665,6 @@ class AlgoliaSync
         return 'index.php?option=com_zoo';
     }
 
-
     private function getDataForCategory(?Category $category = null, bool $pathway = true): ?array
     {
         if (!$category) {
@@ -652,10 +680,10 @@ class AlgoliaSync
 
         $image = $category->getParams()->get('content.teaser_image');
         $data = [
-            'id'   => $category->id,
-            'name' => [],
-            'url' => [],
-            'image' =>  $image ? '/' . $image : null
+            'id'    => $category->id,
+            'name'  => [],
+            'url'   => [],
+            'image' => $image ? '/' . $image : null
         ];
 
         if ($pathway) {
@@ -673,7 +701,7 @@ class AlgoliaSync
         return $data;
     }
 
-    function array_flatten(array $array): array
+    public function array_flatten(array $array): array
     {
         $return = [];
 
@@ -682,5 +710,312 @@ class AlgoliaSync
         });
 
         return $return;
+    }
+
+    private function loadRouterLanguageRules(): void
+    {
+        $this->app = CMSApplication::getInstance('site');
+        $this->mode_sef = $this->app->get('sef', 0);
+        $this->lang_codes = LanguageHelper::getLanguages('lang_code');
+        $this->default_lang = ComponentHelper::getParams('com_languages')->get('site', 'en-GB');
+        $this->current_lang = isset($this->lang_codes[$this->default_lang]) ? $this->default_lang : 'en-GB';
+
+        $this->params = (new Registry(PluginHelper::getPlugin('system', 'languagefilter')->params));
+
+        // We need to make sure we are always using the site router, even if the language plugin is executed in admin app.
+        $router = $this->app->getRouter('site');
+
+        // Attach build rules for language SEF.
+        $router->attachBuildRule([
+            $this,
+            'preprocessBuildRule'
+        ], Router::PROCESS_BEFORE);
+
+        $router->attachBuildRule([
+            $this,
+            'buildRule'
+        ], Router::PROCESS_BEFORE);
+        $router->attachBuildRule([
+            $this,
+            'postprocessSEFBuildRule'
+        ], Router::PROCESS_AFTER);
+
+        // Attach parse rule.
+        $router->attachParseRule(array(
+            $this,
+            'parseRule'
+        ), Router::PROCESS_BEFORE);
+    }
+
+    /**
+     * Add build preprocess rule to router.
+     *
+     * @param Router  &$router Router object.
+     * @param Uri     &$uri Uri object.
+     *
+     * @return  void
+     *
+     * @since   3.4
+     */
+    public function preprocessBuildRule(&$router, &$uri)
+    {
+        $lang = $uri->getVar('lang', $this->current_lang);
+
+        if (isset($this->sefs[$lang])) {
+            $lang = $this->sefs[$lang]->lang_code;
+        }
+
+        $uri->setVar('lang', $lang);
+    }
+
+    /**
+     * Add build rule to router.
+     *
+     * @param Router  &$router Router object.
+     * @param Uri     &$uri Uri object.
+     *
+     * @return  void
+     *
+     * @since   1.6
+     */
+    public function buildRule(&$router, &$uri)
+    {
+        $lang = $uri->getVar('lang');
+
+        if (isset($this->lang_codes[$lang])) {
+            $sef = $this->lang_codes[$lang]->sef;
+        } else {
+            $sef = $this->lang_codes[$this->current_lang]->sef;
+        }
+
+        if (!$this->params->get('remove_default_prefix', 0)
+            || $lang !== $this->default_lang
+            || $lang !== $this->current_lang) {
+            $uri->setPath($uri->getPath() . '/' . $sef . '/');
+        }
+    }
+
+    /**
+     * postprocess build rule for SEF URLs
+     *
+     * @param Router  &$router Router object.
+     * @param Uri     &$uri Uri object.
+     *
+     * @return  void
+     *
+     * @since   3.4
+     */
+    public function postprocessSEFBuildRule(&$router, &$uri)
+    {
+        $uri->delVar('lang');
+    }
+
+    /**
+     * Add parse rule to router.
+     *
+     * @param Router  &$router Router object.
+     * @param Uri     &$uri Uri object.
+     *
+     * @since   1.6
+     */
+    public function parseRule(&$router, &$uri)
+    {
+        // Did we find the current and existing language yet?
+        $found = false;
+
+        // Are we in SEF mode or not?
+        if ($this->mode_sef) {
+            $path = $uri->getPath();
+            $parts = explode('/', $path);
+
+            $sef = StringHelper::strtolower($parts[0]);
+
+            // Do we have a URL Language Code ?
+            if (!isset($this->sefs[$sef])) {
+                // Check if remove default URL language code is set
+                if ($this->params->get('remove_default_prefix', 0)) {
+                    if ($parts[0]) {
+                        // We load a default site language page
+                        $lang_code = $this->default_lang;
+                    } else {
+                        // We check for an existing language cookie
+                        $lang_code = $this->getLanguageCookie();
+                    }
+                } else {
+                    $lang_code = $this->getLanguageCookie();
+                }
+
+                // No language code. Try using browser settings or default site language
+                if (!$lang_code && $this->params->get('detect_browser', 0) == 1) {
+                    $lang_code = LanguageHelper::detectLanguage();
+                }
+
+                if (!$lang_code) {
+                    $lang_code = $this->default_lang;
+                }
+
+                if ($lang_code === $this->default_lang && $this->params->get('remove_default_prefix', 0)) {
+                    $found = true;
+                }
+            } else {
+                // We found our language
+                $found = true;
+                $lang_code = $this->sefs[$sef]->lang_code;
+
+                // If we found our language, but it's the default language and we don't want a prefix for that, we are on a wrong URL.
+                // Or we try to change the language back to the default language. We need a redirect to the proper URL for the default language.
+                if ($lang_code === $this->default_lang && $this->params->get('remove_default_prefix', 0)) {
+                    // Create a cookie.
+                    $this->setLanguageCookie($lang_code);
+
+                    $found = false;
+                    array_shift($parts);
+                    $path = implode('/', $parts);
+                }
+
+                // We have found our language and the first part of our URL is the language prefix
+                if ($found) {
+                    array_shift($parts);
+
+                    // Empty parts array when "index.php" is the only part left.
+                    if (count($parts) === 1 && $parts[0] === 'index.php') {
+                        $parts = array();
+                    }
+
+                    $uri->setPath(implode('/', $parts));
+                }
+            }
+        } // We are not in SEF mode
+        else {
+            $lang_code = $this->getLanguageCookie();
+
+            if (!$lang_code && $this->params->get('detect_browser', 1)) {
+                $lang_code = LanguageHelper::detectLanguage();
+            }
+
+            if (!isset($this->lang_codes[$lang_code])) {
+                $lang_code = $this->default_lang;
+            }
+        }
+
+        $lang = $uri->getVar('lang', $lang_code);
+
+        if (isset($this->sefs[$lang])) {
+            // We found our language
+            $found = true;
+            $lang_code = $this->sefs[$lang]->lang_code;
+        }
+
+        // We are called via POST or the nolangfilter url parameter was set. We don't care about the language
+        // and simply set the default language as our current language.
+        if ($this->app->input->getMethod() === 'POST'
+            || $this->app->input->get('nolangfilter', 0) == 1
+            || count($this->app->input->post) > 0
+            || count($this->app->input->files) > 0) {
+            $found = true;
+
+            if (!isset($lang_code)) {
+                $lang_code = $this->getLanguageCookie();
+            }
+
+            if (!$lang_code && $this->params->get('detect_browser', 1)) {
+                $lang_code = LanguageHelper::detectLanguage();
+            }
+
+            if (!isset($this->lang_codes[$lang_code])) {
+                $lang_code = $this->default_lang;
+            }
+        }
+
+        // We have not found the language and thus need to redirect
+        if (!$found) {
+            // Lets find the default language for this user
+            if (!isset($lang_code) || !isset($this->lang_codes[$lang_code])) {
+                $lang_code = false;
+
+                if ($this->params->get('detect_browser', 1)) {
+                    $lang_code = LanguageHelper::detectLanguage();
+
+                    if (!isset($this->lang_codes[$lang_code])) {
+                        $lang_code = false;
+                    }
+                }
+
+                if (!$lang_code) {
+                    $lang_code = $this->default_lang;
+                }
+            }
+
+            if ($this->mode_sef) {
+                // Use the current language sef or the default one.
+                if ($lang_code !== $this->default_lang
+                    || !$this->params->get('remove_default_prefix', 0)) {
+                    $path = $this->lang_codes[$lang_code]->sef . '/' . $path;
+                }
+
+                $uri->setPath($path);
+
+                if (!$this->app->get('sef_rewrite')) {
+                    $uri->setPath('index.php/' . $uri->getPath());
+                }
+
+                $redirectUri = $uri->base() . $uri->toString(array(
+                        'path',
+                        'query',
+                        'fragment'
+                    ));
+            } else {
+                $uri->setVar('lang', $this->lang_codes[$lang_code]->sef);
+                $redirectUri = $uri->base() . 'index.php?' . $uri->getQuery();
+            }
+
+            // Set redirect HTTP code to "302 Found".
+            $redirectHttpCode = 302;
+
+            // If selected language is the default language redirect code is "301 Moved Permanently".
+            if ($lang_code === $this->default_lang) {
+                $redirectHttpCode = 301;
+
+                // We cannot cache this redirect in browser. 301 is cacheable by default so we need to force to not cache it in browsers.
+                $this->app->setHeader('Expires', 'Wed, 17 Aug 2005 00:00:00 GMT', true);
+                $this->app->setHeader('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT', true);
+                $this->app->setHeader('Cache-Control', 'no-store, no-cache, must-revalidate', false);
+                $this->app->sendHeaders();
+            }
+
+            // Redirect to language.
+            $this->app->redirect($redirectUri, $redirectHttpCode);
+        }
+
+        // We have found our language and now need to set the cookie and the language value in our system
+        $array = array('lang' => $lang_code);
+        $this->current_lang = $lang_code;
+
+        // Set the request var.
+        $this->app->input->set('language', $lang_code);
+        $this->app->set('language', $lang_code);
+        $language = $this->app->getLanguage();
+
+        if ($language->getTag() !== $lang_code) {
+            $language_new = Language::getInstance($lang_code, (bool)$this->app->get('debug_lang'));
+
+            foreach ($language->getPaths() as $extension => $files) {
+                if (strpos($extension, 'plg_system') !== false) {
+                    $extension_name = substr($extension, 11);
+
+                    $language_new->load($extension, JPATH_ADMINISTRATOR)
+                    || $language_new->load($extension, JPATH_PLUGINS . '/system/' . $extension_name);
+
+                    continue;
+                }
+
+                $language_new->load($extension);
+            }
+
+            Factory::$language = $language_new;
+            $this->app->loadLanguage($language_new);
+        }
+
+        return $array;
     }
 }
