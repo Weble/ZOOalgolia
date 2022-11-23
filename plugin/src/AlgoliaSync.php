be3ee777706dbe3922df70caf9b0c164f1f0fed4
davide <davide@weble.it>
2022-11-23 16:58:54 +0100

    wip

--- plugin/src/AlgoliaSync.php
+++ plugin/src/AlgoliaSync.php
@@ -175,16 +175,16 @@ class AlgoliaSync
 
         $this->categories = $application->getCategoryTree();
         $data = [
-            'url'          => [],
-            'category_ids' => array_filter(array_merge($item->getRelatedCategoryIds(), $this->array_flatten(array_map(function ($id) {
-            'id'           => $item->id,
+            'id'  => $item->id,
+            'url' => [],
+            'category_ids' => array_filter(array_merge($item->getRelatedCategoryIds(), $this->array_flatten(array_map(function($id) {
                 /** @var Category $category */
                 $category = $this->categories[$id] ?? null;
                 if (!$category) {
                     return [];
                 }
 
-                return array_map(function ($parent) {
+                return array_map(function($parent) {
                     return $parent->id;
                 }, $category->getPathway());
 
